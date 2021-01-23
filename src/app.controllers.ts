00534a52859a613fb749e5e3ee1d0a9d3079b2c8
presuit <chldudgh8256@naver.com>
2021-01-23 23:38:10 +0900

    uplaods

--- src/app.controllers.ts
+++ src/app.controllers.ts
@@ -1,10 +1,10 @@
 import {
   Controller,
   Post,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import * as AWS from 'aws-sdk';
 
 const BUCKET_NAME = 'ckeditor-image-uploader';
@@ -12,8 +12,8 @@ const BUCKET_NAME = 'ckeditor-image-uploader';
 @Controller('/uploads')
 export class AppControllers {
   @Post('')
-  @UseInterceptors(FileInterceptor('upload'))
-  async uploadImage(@UploadedFile() upload) {
+  @UseInterceptors(FilesInterceptor('uploads'))
+  async uploadImage(@UploadedFiles() uploads) {
     AWS.config.update({
       credentials: {
         accessKeyId: process.env.AWS_S3_ACCESSKEY_ID,
@@ -21,23 +21,31 @@ export class AppControllers {
       },
     });
     try {
-      const objectName = Date.now() + upload.originalname;
-      await new AWS.S3()
-        .putObject({
-          Body: upload.buffer,
-          Bucket: BUCKET_NAME,
-          Key: objectName,
-          ACL: 'public-read',
-        })
-        .promise();
-      const fileUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      console.log(uploads);
+      const uploadContainer = [];
 
-      return {
-        uploaded: true,
-        url: fileUrl,
-      };
+      for (const eachUpload of uploads) {
+        const objectName = Date.now() + eachUpload.originalname;
+        await new AWS.S3()
+          .putObject({
+            Body: eachUpload.buffer,
+            Bucket: BUCKET_NAME,
+            Key: objectName,
+            ACL: 'public-read',
+          })
+          .promise();
+        const fileUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+        uploadContainer.push({ uploaded: true, url: fileUrl });
+      }
+
+      console.log(uploadContainer);
+      return uploadContainer;
     } catch (error) {
       console.log(error);
+      return {
+        uploaded: false,
+        url: null,
+      };
     }
   }
 }
